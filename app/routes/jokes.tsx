137a6fccce368fc426f5f72b60530823b82abd17
Alexandre <rigao.alexandre@gmail.com>
2022-09-01 19:26:33 +1000

    add logout; add register;

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,10 +1,12 @@
 /** @format */
 
-import type {LinksFunction, LoaderFunction} from 'remix';
+import type {User} from '@prisma/client';
+import type {LinksFunction, LoaderFunction} from '@remix-run/node';
 import {json} from '@remix-run/node';
-import {Outlet, Link, useLoaderData} from 'remix';
+import {Link, Outlet, useLoaderData} from '@remix-run/react';
 
 import {db} from '~/utils/db.server';
+import {getUser} from '~/utils/session.server';
 import stylesUrl from '~/styles/jokes.css';
 
 export const links: LinksFunction = () => {
@@ -12,25 +14,26 @@ export const links: LinksFunction = () => {
 };
 
 type LoaderData = {
+   user: Awaited<ReturnType<typeof getUser>>;
    jokeListItems: Array<{id: string; name: string}>;
 };
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({request}) => {
+   const jokeListItems = await db.joke.findMany({
+      take: 5,
+      orderBy: {createdAt: 'desc'},
+      select: {id: true, name: true},
+   });
+   const user = await getUser(request);
+
    const data: LoaderData = {
-      jokeListItems: await db.joke.findMany({
-         take: 5,
-         select: {id: true, name: true},
-         orderBy: {createdAt: 'desc'},
-      }),
+      user,
+      jokeListItems,
    };
    return json(data);
 };
 
 export default function JokesRoute() {
-   /**
-    * TODO: Network Type Safety
-    * @see https://remix.run/docs/en/v1/tutorials/jokes#network-type-safety
-    */
    const data = useLoaderData<LoaderData>();
 
    return (
@@ -43,6 +46,18 @@ export default function JokesRoute() {
                      <span className='logo-medium'>J🤪KES</span>
                   </Link>
                </h1>
+               {data.user ? (
+                  <div className='user-info'>
+                     <span>{`Hi ${data.user.username}`}</span>
+                     <form action='/logout' method='post'>
+                        <button type='submit' className='button'>
+                           Logout
+                        </button>
+                     </form>
+                  </div>
+               ) : (
+                  <Link to='/login'>Login</Link>
+               )}
             </div>
          </header>
          <main className='jokes-main'>
@@ -51,9 +66,6 @@ export default function JokesRoute() {
                   <Link to='.'>Get a random joke</Link>
                   <p>Here are a few more jokes to check out:</p>
                   <ul>
-                     {/* <li>
-                        <Link to='some-joke-id'>Hippo</Link>
-                     </li> */}
                      {data.jokeListItems.map((joke) => (
                         <li key={joke.id}>
                            <Link to={joke.id}>{joke.name}</Link>
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -5,7 +5,7 @@ import {json} from '@remix-run/node';
 import {useActionData, Link, useSearchParams} from '@remix-run/react';
 
 import {db} from '~/utils/db.server';
-import {login, createUserSession} from '~/utils/session.server';
+import {login, createUserSession, register} from '~/utils/session.server';
 import stylesUrl from '~/styles/login.css';
 
 export const links: LinksFunction = () => {
@@ -91,12 +91,14 @@ export const action: ActionFunction = async ({request}) => {
                formError: `User with username ${username} already exists`,
             });
          }
-         // create the user
-         // create their session and redirect to /jokes
-         return badRequest({
-            fields,
-            formError: 'Not implemented',
-         });
+         const user = await register({username, password});
+         if (!user) {
+            return badRequest({
+               fields,
+               formError: `Something went wrong trying to create a new user.`,
+            });
+         }
+         return createUserSession(user.id, redirectTo);
       }
       default: {
          return badRequest({
